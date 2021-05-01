f74cf50d27b58f039f9cf73818f4316378e56289
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2021-05-01 20:06:05 +0300

    feat: add simple login system

--- src/components/HeaderComponents/Header.tsx
+++ src/components/HeaderComponents/Header.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import './Header.scss'
+import {LOGIN, LOGOUT} from '../../constants/labels'
 
 const Header: React.FC = () => {
+    const [isLogedIn, setIsLoggedIn] = useState<boolean>(false)
+
+    const login = useCallback((): void => {
+        const user = {
+            name: 'admin'
+        }
+        if(!isLogedIn) {
+            localStorage.setItem('user', JSON.stringify(user))
+        } else {
+            localStorage.removeItem('user')
+        }
+        setIsLoggedIn(!isLogedIn)
+    }, [isLogedIn])
+
+    useEffect(() => {
+        const hasUser = (localStorage.getItem('user') === null) ? false: true;
+        setIsLoggedIn(hasUser)
+    }, [])
+
     return (
-        <header>
-            <h1>Hi</h1>
+        <header className='f-c'>
+            <div className='header-content f-b'>
+                <h1>Museum logo</h1>
+                <div className='auth-wrap'>
+                    {isLogedIn ?
+                        (<button onClick={login}><p>{LOGOUT}</p></button>)
+                        : (<button onClick={login}><p>{LOGIN}</p></button>)
+                    }
+                </div> 
+            </div>
         </header>
     )
 }
--- src/constants/labels.tsx
+++ src/constants/labels.tsx
@@ -1,1 +1,3 @@
 export const PROJECT_NAME = 'Онлайн галлерея ретро техники'
+export const LOGIN = 'Login'
+export const LOGOUT = 'Logout'
